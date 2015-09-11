d51553e1d5c4b15b15472a7aff9838fc142bbdcf
unknown <x15g020@PC505N007.chiba-fjb.ac.jp>
2015-09-11 12:40:41 +0900

    しょうじ

--- src/test2/test01.java
+++ src/test2/test01.java
@@ -5,7 +5,9 @@ public class test01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
   System.out.println("ｇほｇ");
- System.out.println("a");
+  System.out.println("a");
+  System.out.println("キネクトほしい");
+
 
 }
 }
