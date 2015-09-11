47612025b7b210e980adaaf540b9c292799dbfeb
unknown <x15g008@PC505N001.chiba-fjb.ac.jp>
2015-09-11 11:41:04 +0900

    追加

--- src/test2/test01.java
+++ src/test2/test01.java
@@ -4,7 +4,8 @@ public class test01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-  System.out.println("ｇふｇ");
-	}
+  System.out.println("ｇほｇ");
+ System.out.println("a");
 
+}
 }
